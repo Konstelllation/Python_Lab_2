9ed3103976911f66b0cbabdbbd1420d5d3000afb
VitaliyPitsa <harchenkobo2003@gmail.com>
2022-09-26 10:25:24 +0300

    Выполнил задание №2

---
+++ Zadanie_2.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+        string1 = set(input('Введите первую строку: ').lower())
+        string2 = set(input('Введите вторую строку: ').lower())
+        print(string2.intersection(string1))
